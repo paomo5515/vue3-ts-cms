89b9ffad7b5e48468769121c32537d8399bc927c
PaoMo <paomo5515@qq.com>
2022-06-19 21:03:02 +0800

    style(all): project initialization completed

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,4 @@ import App from "./App.vue"
 import router from "./router"
 import store from "./store"
 
-console.log("hahah")
 createApp(App).use(store).use(router).mount("#app")
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
 const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  configureWebpack: {
+    resolve: {
+      alias: {
+        components: "@/components"
+      }
+    }
+  },
   devServer: {
     port: 6060,
     host: "127.0.0.1",
